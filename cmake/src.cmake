eea4998228f04b550e5409997131f43fd4ee218b
Trevor Mellon <trevormellon1@gmail.com>
2018-01-19 15:53:30 +0000

    source grouping

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -1,6 +1,17 @@
 
 SET (SRC_T 
 	${PROJECT_SOURCE_DIR}/src/tmdb/ApiGetJson.cpp
+	
+)
+
+SET (INC_T
+	${PROJECT_SOURCE_DIR}/include/tmdb/ApiGetJson.h
+	${PROJECT_SOURCE_DIR}/include/tmdb/util.h
+	
+	${PROJECT_SOURCE_DIR}/include/tmdb/config.h
+)
+
+SET (SRC_MOVIE_T
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/AlternativeTitle.cpp	
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/CastNCrew.cpp
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/Keywords.cpp
@@ -8,19 +19,21 @@ SET (SRC_T
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/Videos.cpp	
 )
 
-SET (INC_T
-	${PROJECT_SOURCE_DIR}/include/tmdb/ApiGetJson.h
-	${PROJECT_SOURCE_DIR}/include/tmdb/util.h
+SET(INC_MOVIE_T
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/AlternativeTitle.h	
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/CastNCrew.h
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/Keywords.h
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/Movie.h
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/Types.h
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/Videos.h
-	${CMAKE_BINARY_DIR}/include/tmdb/config.h
 )
 
 include_directories(${CMAKE_BINARY_DIR}/include/)
 include_directories(inl)
 
-SET (SOURCES ${SRC_T} ${INC_T} )
+SET (SOURCES ${SRC_T} ${INC_T} ${SRC_MOVIE_T} ${INC_MOVIE_T})
+
+SOURCE_GROUP("include" FILES ${INC_T})
+SOURCE_GROUP("source" FILES ${SRC_T})
+SOURCE_GROUP("include\\movie" FILES ${INC_MOVIE_T})
+SOURCE_GROUP("source\\movie" FILES ${SRC_MOVIE_T})
