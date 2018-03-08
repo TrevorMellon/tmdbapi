337070d042b6af7edcb7c754657879ac2db5ad7f
Trevor Mellon <trevormellon1@gmail.com>
2018-03-08 13:08:28 +0000

    change and add new files

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -17,18 +17,27 @@ SET (INC_T
 	${PROJECT_SOURCE_DIR}/include/tmdb/config.h
 )
 
-SET (COMMON_INC_T
-	${PROJECT_SOURCE_DIR}/include/tmdb/common/ProductionCompany.h
-	${PROJECT_SOURCE_DIR}/include/tmdb/common/ProductionCountry.h
-	${PROJECT_SOURCE_DIR}/include/tmdb/common/Languages.h
+SET (COMMON_SRC_T
+	${PROJECT_SOURCE_DIR}/src/tmdb/common/CastAndCrew.cpp
+)
+
+SET(COMMON_INC_T
 	${PROJECT_SOURCE_DIR}/include/tmdb/common/CastAndCrew.h
-	${PROJECT_SOURCE_DIR}/include/tmdb/common/People.h
-	${PROJECT_SOURCE_DIR}/include/tmdb/common/Genre.h
+)
+
+SET (COMMON_TYPES_INC_T
+	${PROJECT_SOURCE_DIR}/include/tmdb/common/types/ProductionCompany.h
+	${PROJECT_SOURCE_DIR}/include/tmdb/common/types/ProductionCountry.h
+	${PROJECT_SOURCE_DIR}/include/tmdb/common/types/Languages.h
+	${PROJECT_SOURCE_DIR}/include/tmdb/common/types/CastAndCrew.h
+	${PROJECT_SOURCE_DIR}/include/tmdb/common/types/People.h
+	${PROJECT_SOURCE_DIR}/include/tmdb/common/types/Genre.h
+	${PROJECT_SOURCE_DIR}/include/tmdb/common/types/Types.h
 )
 
 SET (SRC_MOVIE_T
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/AlternativeTitle.cpp	
-	${PROJECT_SOURCE_DIR}/src/tmdb/movie/CastAndCrew.cpp
+	#${PROJECT_SOURCE_DIR}/src/tmdb/movie/CastAndCrew.cpp
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/Keywords.cpp
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/Movie.cpp
 	${PROJECT_SOURCE_DIR}/src/tmdb/movie/Videos.cpp	
@@ -36,7 +45,7 @@ SET (SRC_MOVIE_T
 
 SET(INC_MOVIE_T
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/AlternativeTitle.h	
-	${PROJECT_SOURCE_DIR}/include/tmdb/movie/CastAndCrew.h
+	#${PROJECT_SOURCE_DIR}/include/tmdb/movie/CastAndCrew.h
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/Keywords.h
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/Movie.h
 	${PROJECT_SOURCE_DIR}/include/tmdb/movie/Types.h
@@ -52,7 +61,10 @@ SET (INC_TV_T
 	${PROJECT_SOURCE_DIR}/include/tmdb/tv/Types.h
 )
 
-SET (SOURCES ${SRC_T} ${INC_T} ${SRC_MOVIE_T} ${INC_MOVIE_T} ${SRC_TV_T} ${INC_TV_T} ${COMMON_INC_T})
+SET (SOURCES ${SRC_T} ${INC_T} ${SRC_MOVIE_T} ${INC_MOVIE_T}
+	${SRC_TV_T} ${INC_TV_T} ${COMMON_TYPES_INC_T}
+	${COMMON_SRC_T} ${COMMON_INC_T}
+	)
 
 SOURCE_GROUP("include" FILES ${INC_T})
 SOURCE_GROUP("source" FILES ${SRC_T})
@@ -60,4 +72,4 @@ SOURCE_GROUP("include\\movie" FILES ${INC_MOVIE_T})
 SOURCE_GROUP("source\\movie" FILES ${SRC_MOVIE_T})
 SOURCE_GROUP("include\\tv" FILES ${INC_TV_T})
 SOURCE_GROUP("source\\tv" FILES ${SRC_TV_T})
-SOURCE_GROUP("include\\common" FILES ${COMMON_INC_T})
+SOURCE_GROUP("include\\common\\types" FILES ${COMMON_TYPES_INC_T})
