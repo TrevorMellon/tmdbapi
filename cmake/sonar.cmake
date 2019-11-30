1b04aff35da97b2d7394d9df62683d4e6204ad0e
Codified <codified@protonmail.com>
2019-11-30 20:14:41 +0000

    Update sonar check, cppcheck and vera++

--- cmake/sonar.cmake
+++ cmake/sonar.cmake
@@ -1,4 +1,4 @@
-find_file(VERAPP NAMES vera++.exe)
+find_file(VERAPP NAMES vera++ vera++.exe)
 
 if (${VERAPP} MATCHES VERAPP-NOTFOUND)
 	message("-- vera++ Not Found, test disabled")
@@ -14,12 +14,12 @@ else(${VERAPP} MATCHES VERAPP-NOTFOUND)
 	
 	set(VERARULES -R F001 -R L001 -R L003 -R L004 -R L005 -R L006 -R T001 -R T002 -R T003 -R T004 -R T005 -R T006 -R T007 -R T008 -R T009 -R T010 -R T011 -R T013 -R T015 -R T016 -R T017 -R T018 -R T019)
 	
-	add_custom_target(verastyle  vera++.exe ${VERARULES}  -c ${PROJECT_SOURCE_DIR}/sonar/vera++-report.xml --warning ${verasrcs})
+        add_custom_target(verastyle  ${VERAPP} ${VERARULES}  -c ${PROJECT_SOURCE_DIR}/sonar/vera++-report.xml --warning ${verasrcs})
 	#message("${VERAPP}  -x ${PROJECT_SOURCE_DIR}/vera++-report.xml --warning ${verasrcs_STR}")
 endif(${VERAPP} MATCHES VERAPP-NOTFOUND)
 
 
-find_file(CPPCHECK NAMES cppcheck.exe)
+find_file(CPPCHECK NAMES cppcheck cppcheck.exe)
 
 if (${CPPCHECK} MATCHES CPPCHECK-NOTFOUND)
 	message("-- cppcheck Not Found, test disabled")
@@ -31,7 +31,7 @@ else(${CPPCHECK} MATCHES CPPCHECK-NOTFOUND)
 		
 	SET (CPPC_INC -I${Boost_INCLUDE_DIRS} -I${ICU_INCLUDE_DIRS})
 	
-	if( RAPIDJSON_FOUND AND USE_RAPIDJSON)
+        if( RAPIDJSON_FOUND)
 		SET (CPPC_INC ${CPPC_INC} -I${RAPIDJSON_INCLUDE_DIRS})
 	endif()
 	
@@ -45,16 +45,18 @@ else(${CPPCHECK} MATCHES CPPCHECK-NOTFOUND)
 	
 	message("-- cppcheck - Found")
 	message("-- cppcheck: ${CPPCHECK}")
-	SET ( VCINC "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\include")
-	SET ( CPPC_DEF -D_WIN32 -DWINDOWS -D_NDEBUG -DNDEBUG -D_MSC_VER -DBOOST-ALL-NO-LIB)
-	SET ( CPPC_INC ${CPPC_INC} -I${VCINC} )
-	
-	SET ( CPPC_INC -I${PROJECT_SOURCE_DIR}/include -I${PROJECT_SOURCE_DIR}/inl -I${PROJECT_SOURCE_DIR}/src/db/private)
+        if(WIN32)
+            SET ( VCINC "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\include")
+            SET ( CPPC_DEF -D_WIN32 -DWINDOWS -D_NDEBUG -DNDEBUG -D_MSC_VER -DBOOST-ALL-NO-LIB)
+            SET ( CPPC_INC ${CPPC_INC} -I${VCINC} )
+        endif(WIN32)
+
+        SET ( CPPC_INC -I${PROJECT_SOURCE_DIR}/include)
 	
 	SET(CPPCHK_FILE ${PROJECT_SOURCE_DIR}/sonar/cppcheck-results.xml)
 	SET(CPPCHK_OPTS --force ${CPPC_INC}  --xml --library=windows --platform=win32A --std=c++11 --enable=all ${cpcsrcs})	
 	
-	add_custom_target(cppcheck COMMAND ${PROJECT_SOURCE_DIR}/cppcheckscript.bat ${CPPCHK_FILE} "${PROJECT_SOURCE_DIR}/src" "${CPPC_INC}" )
+        add_custom_target(cppcheck COMMAND ${CPPCHECK} ${CPPCHK_OPTS} "${PROJECT_SOURCE_DIR}/src" "${CPPC_INC}" 2> ${CPPCHK_FILE} )
 
 endif(${CPPCHECK} MATCHES CPPCHECK-NOTFOUND)
 
