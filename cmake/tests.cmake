d53f0b9f845ad3220f128a954b93109711a710b9
Codified <codified@protonmail.com>
2024-07-20 11:04:53 +0100

    cmake fetch google-test

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -4,13 +4,22 @@ find_package(GTest)
 
 SET(TEST_SRC ${PROJECT_SOURCE_DIR}/tests/test.cpp)
 
+include(FetchContent)
+FetchContent_Declare(
+  googletest
+  URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+)
+
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+
 add_executable(tmdbtest ${TEST_SRC}
 )
 
 target_link_libraries(tmdbtest ${PROJECT_NAME})
 
 target_link_libraries(tmdbtest 
-			${GTEST_BOTH_LIBRARIES} 
+			GTest::gtest_main
 			${Boost_LIBRARIES}
 			${ICU_LIBRARIES}
 			)
